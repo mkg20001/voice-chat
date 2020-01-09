1dae39d542ad75e925deb638f09c9399e2601d9a
Dirk Krause <dirkk0@googlemail.com>
2020-01-09 10:07:13 +0100

    serve index.html

--- core/express/index.js
+++ core/express/index.js
@@ -3,6 +3,7 @@ const options = {
   key: fs.readFileSync('testcert/key.pem'),
   cert: fs.readFileSync('testcert/cert.pem')
 };
+app.use('/', express.static(__dirname + '/../../client'));
 const server=global.server=https.createServer(options,app).listen(9980);
 server.on("listen",function() {
   mainlog("Server","@",9980);
